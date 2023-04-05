8e64cedb53245cea2ba84772c772f9288376142a
Franci Penov <francip@gmail.com>
2023-04-04 23:10:10 -0700

    git2gpt: Create a copy of the SingleTaskListStorage in a separate file, and name it CooperativeTaskListStorage

---
+++ CooperativeTaskListStorage.py
@@ -0,0 +1,18 @@
+class CooperativeTaskListStorage:
+    def __init__(self):
+        self.task_list = []
+
+    def add_task(self, task):
+        self.task_list.append(task)
+
+    def get_task(self, index):
+        return self.task_list[index]
+
+    def remove_task(self, index):
+        del self.task_list[index]
+
+    def get_all_tasks(self):
+        return self.task_list
+
+    def get_task_count(self):
+        return len(self.task_list)
