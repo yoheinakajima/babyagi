752e74d7e794ab0ba6bb31b24844469e43afd77c
MalikMAlna <mmikema.developer@gmail.com>
2023-04-07 13:02:23 -0400

    Formatting, removing unnecessary import, and general cleanup for consistency

--- babyagi.py
+++ babyagi.py
@@ -7,7 +7,6 @@ import sys
 from collections import deque
 from typing import Dict, List
 from dotenv import load_dotenv
-import os
 
 # Set Variables
 load_dotenv()
@@ -20,7 +19,7 @@ OPENAI_API_MODEL = os.getenv("OPENAI_API_MODEL", "gpt-3.5-turbo")
 assert OPENAI_API_MODEL, "OPENAI_API_MODEL environment variable is missing from .env"
 
 if "gpt-4" in OPENAI_API_MODEL.lower():
-    print(f"\033[91m\033[1m"+"\n*****USING GPT-4. POTENTIALLY EXPENSIVE. MONITOR YOUR COSTS*****"+"\033[0m\033[0m")
+    print("\033[91m\033[1m"+"\n*****USING GPT-4. POTENTIALLY EXPENSIVE. MONITOR YOUR COSTS*****" + "\033[0m\033[0m")
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "")
 assert PINECONE_API_KEY, "PINECONE_API_KEY environment variable is missing from .env"
@@ -39,7 +38,7 @@ assert OBJECTIVE, "OBJECTIVE environment variable is missing from .env"
 YOUR_FIRST_TASK = os.getenv("FIRST_TASK", "")
 assert YOUR_FIRST_TASK, "FIRST_TASK environment variable is missing from .env"
 
-#Print OBJECTIVE
+# Print OBJECTIVE
 print("\033[96m\033[1m"+"\n*****OBJECTIVE*****\n"+"\033[0m\033[0m")
 print(OBJECTIVE)
 
@@ -61,12 +60,15 @@ index = pinecone.Index(table_name)
 # Task list
 task_list = deque([])
 
+
 def add_task(task: Dict):
     task_list.append(task)
+
 
 def get_ada_embedding(text):
     text = text.replace("\n", " ")
     return openai.Embedding.create(input=[text], model="text-embedding-ada-002")["data"][0]["embedding"]
+
 
 def openai_call(prompt: str, model: str = OPENAI_API_MODEL, temperature: float = 0.5, max_tokens: int = 100):
     if not model.startswith('gpt-'):
@@ -83,7 +85,7 @@ def openai_call(prompt: str, model: str = OPENAI_API_MODEL, temperature: float =
         return response.choices[0].text.strip()
     else:
         # Use chat completion API
-        messages=[{"role": "user", "content": prompt}]
+        messages = [{"role": "user", "content": prompt}]
         response = openai.ChatCompletion.create(
             model=model,
             messages=messages,
@@ -94,11 +96,13 @@ def openai_call(prompt: str, model: str = OPENAI_API_MODEL, temperature: float =
         )
         return response.choices[0].message.content.strip()
 
+
 def task_creation_agent(objective: str, result: Dict, task_description: str, task_list: List[str]):
     prompt = f"You are an task creation AI that uses the result of an execution agent to create new tasks with the following objective: {objective}, The last completed task has the result: {result}. This result was based on this task description: {task_description}. These are incomplete tasks: {', '.join(task_list)}. Based on the result, create new tasks to be completed by the AI system that do not overlap with incomplete tasks. Return the tasks as an array."
     response = openai_call(prompt)
     new_tasks = response.split('\n')
     return [{"task_name": task_name} for task_name in new_tasks]
+
 
 def prioritization_agent(this_task_id: int):
     global task_list
@@ -118,20 +122,23 @@ def prioritization_agent(this_task_id: int):
             task_name = task_parts[1].strip()
             task_list.append({"task_id": task_id, "task_name": task_name})
 
+
 def execution_agent(objective: str, task: str) -> str:
-    context=context_agent(query=objective, n=5)
-    #print("\n*******RELEVANT CONTEXT******\n")
-    #print(context)
-    prompt =f"You are an AI who performs one task based on the following objective: {objective}.\nTake into account these previously completed tasks: {context}\nYour task: {task}\nResponse:"
+    context = context_agent(query=objective, n=5)
+    # print("\n*******RELEVANT CONTEXT******\n")
+    # print(context)
+    prompt = f"You are an AI who performs one task based on the following objective: {objective}.\nTake into account these previously completed tasks: {context}\nYour task: {task}\nResponse:"
     return openai_call(prompt, temperature=0.7, max_tokens=2000)
+
 
 def context_agent(query: str, n: int):
     query_embedding = get_ada_embedding(query)
     results = index.query(query_embedding, top_k=n, include_metadata=True)
-    #print("***** RESULTS *****")
-    #print(results)
+    # print("***** RESULTS *****")
+    # print(results)
     sorted_results = sorted(results.matches, key=lambda x: x.score, reverse=True)    
     return [(str(item.metadata['task'])) for item in sorted_results]
+
 
 # Add the first task
 first_task = {
@@ -147,15 +154,15 @@ while True:
         # Print the task list
         print("\033[95m\033[1m"+"\n*****TASK LIST*****\n"+"\033[0m\033[0m")
         for t in task_list:
-            print(str(t['task_id'])+": "+t['task_name'])
+            print(str(t['task_id']) + ": " + t['task_name'])
 
         # Step 1: Pull the first task
         task = task_list.popleft()
         print("\033[92m\033[1m"+"\n*****NEXT TASK*****\n"+"\033[0m\033[0m")
-        print(str(task['task_id'])+": "+task['task_name'])
+        print(str(task['task_id']) + ": " + task['task_name'])
 
         # Send to execution function to complete the task based on the context
-        result = execution_agent(OBJECTIVE,task["task_name"])
+        result = execution_agent(OBJECTIVE, task["task_name"])
         this_task_id = int(task["task_id"])
         print("\033[93m\033[1m"+"\n*****TASK RESULT*****\n"+"\033[0m\033[0m")
         print(result)
@@ -164,10 +171,10 @@ while True:
         enriched_result = {'data': result}  # This is where you should enrich the result if needed
         result_id = f"result_{task['task_id']}"
         vector = enriched_result['data']  # extract the actual result from the dictionary
-        index.upsert([(result_id, get_ada_embedding(vector),{"task":task['task_name'],"result":result})])
+        index.upsert([(result_id, get_ada_embedding(vector), {"task": task['task_name'], "result":result})])
 
     # Step 3: Create new tasks and reprioritize task list
-    new_tasks = task_creation_agent(OBJECTIVE,enriched_result, task["task_name"], [t["task_name"] for t in task_list])
+    new_tasks = task_creation_agent(OBJECTIVE, enriched_result, task["task_name"], [t["task_name"] for t in task_list])
 
     for new_task in new_tasks:
         task_id_counter += 1
