9990b26757210c3280d99cae8e945173d8e7eeb2
MalikMAlna <mmikema.developer@gmail.com>
2023-04-07 13:08:53 -0400

    Some more formatting and changing new_tasks split

--- babyagi.py
+++ babyagi.py
@@ -39,7 +39,7 @@ YOUR_FIRST_TASK = os.getenv("FIRST_TASK", "")
 assert YOUR_FIRST_TASK, "FIRST_TASK environment variable is missing from .env"
 
 # Print OBJECTIVE
-print("\033[96m\033[1m"+"\n*****OBJECTIVE*****\n"+"\033[0m\033[0m")
+print("\033[96m\033[1m" + "\n*****OBJECTIVE*****\n" + "\033[0m\033[0m")
 print(OBJECTIVE)
 
 # Configure OpenAI and Pinecone
@@ -100,7 +100,7 @@ def openai_call(prompt: str, model: str = OPENAI_API_MODEL, temperature: float =
 def task_creation_agent(objective: str, result: Dict, task_description: str, task_list: List[str]):
     prompt = f"You are an task creation AI that uses the result of an execution agent to create new tasks with the following objective: {objective}, The last completed task has the result: {result}. This result was based on this task description: {task_description}. These are incomplete tasks: {', '.join(task_list)}. Based on the result, create new tasks to be completed by the AI system that do not overlap with incomplete tasks. Return the tasks as an array."
     response = openai_call(prompt)
-    new_tasks = response.split('\n')
+    new_tasks = response.split('\n') if '\n' in response else [response]
     return [{"task_name": task_name} for task_name in new_tasks]
 
 
