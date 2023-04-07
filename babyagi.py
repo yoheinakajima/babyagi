80e503f80f7717e922415885507b12255a32f0c8
CptCaptain <nilskoch@posteo.de>
2023-04-07 02:25:10 +0200

    exit main loop when no tasks are left

--- babyagi.py
+++ babyagi.py
@@ -137,30 +137,28 @@ first_task = {
 add_task(first_task)
 # Main loop
 task_id_counter = 1
-while True:
-    if task_list:
-        # Print the task list
-        print("\033[95m\033[1m"+"\n*****TASK LIST*****\n"+"\033[0m\033[0m")
-        for task in task_list:
-            print(f"{task['task_id']}: {task['task_name']}")
-
-        # Step 1: Pull the first task
-        task = task_list.popleft()
-        print("\033[92m\033[1m"+"\n*****NEXT TASK*****\n"+"\033[0m\033[0m")
+while task_list:
+    # Print the task list
+    print("\033[95m\033[1m"+"\n*****TASK LIST*****\n"+"\033[0m\033[0m")
+    for task in task_list:
         print(f"{task['task_id']}: {task['task_name']}")
 
-        # Send to execution function to complete the task based on the context
-        result = execution_agent(OBJECTIVE, task["task_name"])
-        this_task_id = int(task["task_id"])
-        print("\033[93m\033[1m"+"\n*****TASK RESULT*****\n"+"\033[0m\033[0m")
-        print(result)
+    # Step 1: Pull the first task
+    task = task_list.popleft()
+    print("\033[92m\033[1m"+"\n*****NEXT TASK*****\n"+"\033[0m\033[0m")
+    print(f"{task['task_id']}: {task['task_name']}")
 
-        # Step 2: Enrich result and store in Pinecone
-        enriched_result = {'data': result}  # This is where you should enrich the result if needed
-        result_id = f"result_{task['task_id']}"
-        vector = enriched_result['data']  # extract the actual result from the dictionary
-        index.upsert([(result_id, get_ada_embedding(vector), {"task":task['task_name'], "result":result})])
+    # Send to execution function to complete the task based on the context
+    result = execution_agent(OBJECTIVE, task["task_name"])
+    this_task_id = int(task["task_id"])
+    print("\033[93m\033[1m"+"\n*****TASK RESULT*****\n"+"\033[0m\033[0m")
+    print(result)
 
+    # Step 2: Enrich result and store in Pinecone
+    enriched_result = {'data': result}  # This is where you should enrich the result if needed
+    result_id = f"result_{task['task_id']}"
+    vector = enriched_result['data']  # extract the actual result from the dictionary
+    index.upsert([(result_id, get_ada_embedding(vector), {"task":task['task_name'], "result":result})])
     # Step 3: Create new tasks and reprioritize task list
     new_tasks = task_creation_agent(OBJECTIVE, enriched_result, task["task_name"], [t["task_name"] for t in task_list])
 
@@ -171,3 +169,6 @@ while True:
     prioritization_agent(this_task_id)
 
     time.sleep(1)  # Sleep before checking the task list again
+
+print(f"No tasks left, objective '{OBJECTIVE}' achieved")
+
