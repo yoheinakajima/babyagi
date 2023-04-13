701609e20091bd6e02e4e1b71e1cbda001c02a18
Josh XT <josh@devxt.com>
2023-04-13 08:01:17 -0400

    Added extensions support and updated prompts

--- babyagi-local.py
+++ babyagi-local.py
@@ -1,18 +1,61 @@
+import os
 import requests
 import time
 from collections import deque
 from typing import Dict, List
+import importlib
 from sentence_transformers import SentenceTransformer
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
-# Update this to your Oobabooga Text Generation Web UI server's IP address.
-# Follow the instructions here to set up the server:
-# https://github.com/oobabooga/text-generation-webui
-# Search Huggingface for vicuna13b-1.1 to find the model.
-OOBA_SERVER = "localhost"
+from dotenv import load_dotenv
+load_dotenv()
 
-OBJECTIVE = "Solve world hunger"
-INITIAL_TASK = "Develop a task list."
+# Server Configuration
+## Update this to your Oobabooga Text Generation Web UI server's IP address.
+## Follow the instructions here to set up the server:
+## https://github.com/oobabooga/text-generation-webui
+## Search Huggingface for vicuna13b-1.1 to find the model for your server.
+OOBA_SERVER = os.getenv("OOBA_SERVER", "localhost")
+
+# Goal configuation
+OBJECTIVE = os.getenv("OBJECTIVE", "Solve world hunger")
+INITIAL_TASK = os.getenv("INITIAL_TASK", "Develop a task list.")
+
+# Extensions support begin
+
+def can_import(module_name):
+    try:
+        importlib.import_module(module_name)
+        return True
+    except ImportError:
+        return False
+
+
+DOTENV_EXTENSIONS = os.getenv("DOTENV_EXTENSIONS", "").split(" ")
+
+# Command line arguments extension
+# Can override any of the above environment variables
+ENABLE_COMMAND_LINE_ARGS = (
+    os.getenv("ENABLE_COMMAND_LINE_ARGS", "false").lower() == "true"
+)
+if ENABLE_COMMAND_LINE_ARGS:
+    if can_import("extensions.argparseext"):
+        from extensions.argparseext import parse_arguments
+
+        OBJECTIVE, INITIAL_TASK, OPENAI_API_MODEL, DOTENV_EXTENSIONS = parse_arguments()
+
+# Load additional environment variables for enabled extensions
+if DOTENV_EXTENSIONS:
+    if can_import("extensions.dotenvext"):
+        from extensions.dotenvext import load_dotenv_extensions
+
+        load_dotenv_extensions(DOTENV_EXTENSIONS)
+
+# TODO: There's still work to be done here to enable people to get
+# defaults from dotenv extensions # but also provide command line
+# arguments to override them
+
+# Extensions support end
 
 def generate_text(prompt):
     response = requests.post(f"http://{OOBA_SERVER}:7860/run/textgen", json={
@@ -45,6 +88,10 @@ def generate_text(prompt):
     }).json()
     return response["data"][0]
 
+# Check if we know what we are doing
+assert OBJECTIVE, "OBJECTIVE environment variable is missing from .env"
+assert INITIAL_TASK, "INITIAL_TASK environment variable is missing from .env"
+
 # Print OBJECTIVE
 print("\033[96m\033[1m" + "\n*****OBJECTIVE*****\n" + "\033[0m\033[0m")
 print(OBJECTIVE)
@@ -61,12 +108,12 @@ def add_task(task: Dict):
     task_list.append(task)
 
 def task_creation_agent(objective: str, result: Dict, task_description: str, task_list: List[str]):
-    prompt = (
-        f"You are an task creation AI that uses the result of an execution agent to create new tasks with the following objective: {objective}, "
-        f"The last completed task has the result: {result}. This result was based on this task description: {task_description}. "
-        f"These are incomplete tasks: {', '.join(task_list)}. "
-        f"Based on the result, create new tasks to be completed by the AI system that do not overlap with incomplete tasks. Return the tasks as an array."
-    )
+    prompt = f"""
+    You are a task creation AI that uses the result of an execution agent to create new tasks with the following objective: {objective},
+    The last completed task has the result: {result}.
+    This result was based on this task description: {task_description}. These are incomplete tasks: {', '.join(task_list)}.
+    Based on the result, create new tasks to be completed by the AI system that do not overlap with incomplete tasks.
+    Return the tasks as an array."""
     new_tasks = generate_text(prompt).strip().split("\n")
     return [{"task_name": task_name} for task_name in new_tasks]
 
@@ -74,13 +121,13 @@ def prioritization_agent(this_task_id: int):
     global task_list
     task_names = [t["task_name"] for t in task_list]
     next_task_id = int(this_task_id) + 1
-    prompt = (
-        f"""You are an task prioritization AI tasked with cleaning the formatting of and reprioritizing the following tasks: {task_names}. """
-        f"""Consider the ultimate objective of your team:{OBJECTIVE}. Do not remove any tasks. Return the result as a numbered list, like:
+    prompt = f"""
+    You are a task prioritization AI tasked with cleaning the formatting of and reprioritizing the following tasks: {task_names}.
+    Consider the ultimate objective of your team:{OBJECTIVE}.
+    Do not remove any tasks. Return the result as a numbered list, like:
     #. First task
     #. Second task
     Start the task list with number {next_task_id}."""
-    )
     new_tasks = generate_text(prompt).strip().split("\n")
     task_list = deque()
     for task_string in new_tasks:
@@ -91,15 +138,37 @@ def prioritization_agent(this_task_id: int):
             task_list.append({"task_id": task_id, "task_name": task_name})
 
 def execution_agent(objective: str, task: str) -> str:
+    """
+    Executes a task based on the given objective and previous context.
+
+    Args:
+        objective (str): The objective or goal for the AI to perform the task.
+        task (str): The task to be executed by the AI.
+
+    Returns:
+        str: The response generated by the AI for the given task.
+
+    """
+
     context = context_agent(query=objective, n=5)
-    prompt = (
-        f"You are an AI who performs one task based on the following objective: {objective}.\n"
-        f"Take into account these previously completed tasks: {context}\n"
-        f"Your task: {task}\nResponse:"
-    )
+    prompt = f"""
+    You are an AI who performs one task based on the following objective: {objective}\n.
+    Take into account these previously completed tasks: {context}\n.
+    Your task: {task}\nResponse:"""
     return generate_text(prompt).strip()
 
 def context_agent(query: str, index: str, n: int):
+    """
+    Retrieves context for a given query from an index of tasks.
+
+    Args:
+        query (str): The query or objective for retrieving context.
+        n (int): The number of top results to retrieve.
+
+    Returns:
+        list: A list of tasks as context for the given query, sorted by relevance.
+
+    """
     results = index.similarity_search_with_score(query, k=n)
     sorted_results = sorted(results, key=lambda x: x[1], reverse=True)
     return [item[0].metadata["task"] for item in sorted_results]
@@ -140,7 +209,12 @@ while True:
         index.add_texts([result], metadatas=[{"task":task["task_name"]}])
 
     # Step 3: Create new tasks and reprioritize task list
-    new_tasks = task_creation_agent(OBJECTIVE, enriched_result, task["task_name"], [t["task_name"] for t in task_list])
+    new_tasks = task_creation_agent(
+        OBJECTIVE,
+        enriched_result,
+        task["task_name"],
+        [t["task_name"] for t in task_list],
+    )
 
     for new_task in new_tasks:
         task_id_counter += 1
