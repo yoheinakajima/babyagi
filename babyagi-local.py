5991a41f3415c3be575bd86209c3e43ef181f8e8
Josh XT <josh@devxt.com>
2023-04-15 08:11:28 -0400

    Updates

--- babyagi-local.py
+++ babyagi-local.py
@@ -82,35 +82,36 @@ if DOTENV_EXTENSIONS:
 # Extensions support end
 
 def generate_text(prompt):
-    params = {
-        'max_new_tokens': 500,
-        'do_sample': True,
-        'temperature': 0.72,
-        'top_p': 0.73,
-        'typical_p': 1,
-        'repetition_penalty': 1.1,
-        'encoder_repetition_penalty': 1.0,
-        'top_k': 0,
-        'min_length': 0,
-        'no_repeat_ngram_size': 0,
-        'num_beams': 1,
-        'penalty_alpha': 0,
-        'length_penalty': 1,
-        'early_stopping': False,
-        'seed': -1,
-        'add_bos_token': True,
-        'custom_stopping_strings': [],
-        'truncation_length': 2048,
-        'ban_eos_token': False,
-    }
-    payload = json.dumps([prompt, params])
-    response = requests.post(f"http://{OOBA_SERVER}:7860/run/textgen", json={
+    response = requests.post("http://localhost:7860/run/textgen", json={
         "data": [
-            payload
+            [
+                prompt,
+                {
+                    'max_new_tokens': 500,
+                    'do_sample': True,
+                    'temperature': 0.7,
+                    'top_p': 0.73,
+                    'typical_p': 1,
+                    'repetition_penalty': 1.1,
+                    'encoder_repetition_penalty': 1.0,
+                    'top_k': 0,
+                    'min_length': 0,
+                    'no_repeat_ngram_size': 0,
+                    'num_beams': 1,
+                    'penalty_alpha': 0,
+                    'length_penalty': 1,
+                    'early_stopping': False,
+                    'seed': -1,
+                    'add_bos_token': True,
+                    'custom_stopping_strings': [],
+                    'truncation_length': 2048,
+                    'ban_eos_token': False,
+                }
+            ]
         ]
     }).json()
     data = response['data'][0]
-    return data.replace("\\n", "\n")
+    return data.replace("\\n", "\n").strip()
 
 # Check if we know what we are doing
 assert OBJECTIVE, "OBJECTIVE environment variable is missing from .env"
--- babyagi.py
+++ babyagi.py
@@ -2,6 +2,7 @@
 import os
 import subprocess
 import time
+import requests
 from collections import deque
 from typing import Dict, List
 import importlib
@@ -144,6 +145,37 @@ def openai_call(
                 cmd = ["llama/main", "-p", prompt]
                 result = subprocess.run(cmd, shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.PIPE, text=True)
                 return result.stdout.strip()
+            if model.startswith("ooba"):
+                response = requests.post("http://localhost:7860/run/textgen", json={
+                    "data": [
+                        [
+                            prompt,
+                            {
+                                'max_new_tokens': max_tokens,
+                                'do_sample': True,
+                                'temperature': temperature,
+                                'top_p': 0.73,
+                                'typical_p': 1,
+                                'repetition_penalty': 1.1,
+                                'encoder_repetition_penalty': 1.0,
+                                'top_k': 0,
+                                'min_length': 0,
+                                'no_repeat_ngram_size': 0,
+                                'num_beams': 1,
+                                'penalty_alpha': 0,
+                                'length_penalty': 1,
+                                'early_stopping': False,
+                                'seed': -1,
+                                'add_bos_token': True,
+                                'custom_stopping_strings': [],
+                                'truncation_length': 2048,
+                                'ban_eos_token': False,
+                            }
+                        ]
+                    ]
+                }).json()
+                data = response['data'][0]
+                return data.replace("\\n", "\n").strip()
             elif not model.startswith("gpt-"):
                 # Use completion API
                 response = openai.Completion.create(
