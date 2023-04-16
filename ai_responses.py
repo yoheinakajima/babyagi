b1c251be8db4fff8316338f78db0970410670375
Brian <46642841+b-chase@users.noreply.github.com>
2023-04-16 18:56:49 -0400

    moved modules to new files

---
+++ ai_responses.py
@@ -0,0 +1,59 @@
+import openai
+import os
+import subprocess
+import time
+
+
+OPENAI_API_MODEL = os.getenv('OPENAI_API_MODEL')
+OPENAI_TEMPERATURE = os.getenv('OPENAI_TEMPERATURE')
+
+
+def get_ada_embedding(text):
+    text = text.replace("\n", " ")
+    return openai.Embedding.create(input=[text], model="text-embedding-ada-002")["data"][0]["embedding"]
+
+
+def openai_call(
+    prompt: str,
+    model: str = OPENAI_API_MODEL,
+    temperature: float = OPENAI_TEMPERATURE,
+    max_tokens: int = 200,
+):
+    while True:
+        try:
+            if model.startswith("llama"):
+                # Spawn a subprocess to run llama.cpp
+                cmd = ["llama/main", "-p", prompt]
+                result = subprocess.run(cmd, shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.PIPE, text=True)
+                return result.stdout.strip()
+            elif not model.startswith("gpt-"):
+                # Use completion API
+                response = openai.Completion.create(
+                    engine=model,
+                    prompt=prompt,
+                    temperature=temperature,
+                    max_tokens=max_tokens,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0,
+                )
+                return response.choices[0].text.strip()
+            else:
+                # Use chat completion API
+                messages = [{"role": "system", "content": prompt}]
+                response = openai.ChatCompletion.create(
+                    model=model,
+                    messages=messages,
+                    temperature=temperature,
+                    max_tokens=max_tokens,
+                    n=1,
+                    stop=None,
+                )
+                return response.choices[0].message.content.strip()
+        except openai.error.RateLimitError:
+            print(
+                "The OpenAI API rate limit has been exceeded. Waiting 10 seconds and trying again."
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        else:
+            break
