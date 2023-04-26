a65272d4828da7b64b749992620480da590116ed
Peter Banda <peter.banda@protonmail.com>
2023-04-26 12:59:49 +0200

    Fixing a typo in README:  -> . Closes #268

--- babyagi.py
+++ babyagi.py
@@ -38,7 +38,7 @@ INSTANCE_NAME = os.getenv("INSTANCE_NAME", os.getenv("BABY_NAME", "BabyAGI"))
 COOPERATIVE_MODE = "none"
 JOIN_EXISTING_OBJECTIVE = False
 
-# Goal configuation
+# Goal configuration
 OBJECTIVE = os.getenv("OBJECTIVE", "")
 INITIAL_TASK = os.getenv("INITIAL_TASK", os.getenv("FIRST_TASK", ""))
 
